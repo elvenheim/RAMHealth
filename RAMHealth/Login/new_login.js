6b5456222a3266606fef542bcfb9b71e705264e6
Rich Diamond <rich.custodio35@gmail.com>
2023-03-24 12:10:24 +0800

    test

---
+++ RAMHealth/Login/new_login.js
@@ -0,0 +1,16 @@
+function show(){
+          var pswrd = document.getElementById('pswrd');
+          var icon = document.querySelector('.fa-eye');
+          if (pswrd.type === "password") {
+           pswrd.type = "text";
+           pswrd.style.marginTop = "20px";
+           icon.style.color = "#293A82";
+          }else{
+           pswrd.type = "password";
+           icon.style.color = "grey";
+          }
+}
+
+function housekeeper(){
+            location.href="/Housekeeper/housekeeper.html";
+}
