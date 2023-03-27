21fb4429beec8ed21b410657719bfe6218d0a957
Rich Diamond <rich.custodio35@gmail.com>
2023-03-27 09:43:19 +0800

    testing

--- RAMHealth/Login/new_login.js
+++ RAMHealth/Login/new_login.js
@@ -1,4 +1,4 @@
-function show(){
+function show_password(){
     var password = document.getElementById('password');
     var icon = document.querySelector('.fa-eye');
     if (password.type === "password") {
@@ -12,5 +12,21 @@ function show(){
 }
 
 function housekeeper(){
-        document.location.href="../Housekeeper/housekeeper.html";
+    document.location.href="../Housekeeper/housekeeper.html";
+}
+
+function air_technician(){
+    document.location.href="../Air Quality Technician/air_technician.html"
+}
+
+function energy_technician(){
+    document.location.href="../Energy Consumption Technician/energy_technician.html"
+}
+
+function admin(){
+    document.location.href="../Administrator/admin.html"
+}
+
+function building_head(){
+    document.location.href="../Building Management Head/building_head.html"
 }
