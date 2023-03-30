84d2ecc32e730ac3ee4e1022c190f55881542886
Rich Diamond <rich.custodio35@gmail.com>
2023-03-30 21:42:18 +0800

    test

--- RAMHealth/Login/new_login.php
+++ RAMHealth/Login/new_login.php
@@ -1,12 +1,24 @@
 <?php 
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    $connection = mysqli_connect("localhost", "root", "");
+    $db = mysqli_select_db($connection, 'ramhealth');
 
-    $conn = new mysqli('localhost', 'root','','user');
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    if(isset($_POST['signin']))
+    {
+        
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        
+        $query = "SELECT * FROM `user` WHERE user_email= '$email' AND user_password= '$password' ";
+        $query_run = mysqli_query($connection, $query);
+        
+        if(mysqli_fetch_array($query_run)>0){
+            echo " You Have Successfully Logged in";
+            exit();
+        }
+        else{
+            echo " You Have Entered Incorrect Password";
+            exit();
+        }
+            
     }
-    echo "Yes";
-
 ?>
